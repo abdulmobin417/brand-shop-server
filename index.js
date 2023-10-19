a1a13f250abc532bca5df3377c7be4cc5732b1f3
AbdulMobin <abdulmobin417@gmail.com>
2023-10-19 12:54:43 +0600

    mongodb connection url set up done with env

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hixyzlt.mongodb.net/?retryWrites=true&w=majority`;
+
 
 
 app.get("/", (req, res) => {
