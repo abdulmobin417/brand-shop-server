e3298e0965036c928030c434f55c57739baa772a
AbdulMobin <abdulmobin417@gmail.com>
2023-10-19 12:55:57 +0600

    conection set up and collection create done

--- index.js
+++ index.js
@@ -11,7 +11,39 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hixyzlt.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const brandsCollection = client.db("brandShop").collection("brands");
+    const carCollection = client.db("brandShop").collection("cars");
+    const usersCollection = client.db("brandShop").collection("users");
+    const cartCollection = client.db("brandShop").collection("carts");
+
+    // filter cars by brand name
+    
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Brand Shop server is running");
